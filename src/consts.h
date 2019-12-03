c818836e87d13d569cc2db860c0ab18770719e5a
ambiesoft <ambiesoft.trueff@gmail.com>
2019-12-03 14:49:13 +0900

    update

--- src/consts.h
+++ src/consts.h
@@ -6,7 +6,7 @@ namespace Consts
 Q_CONSTEXPR char ORGANIZATION[] = "Ambiesoft";
 Q_CONSTEXPR char APPDOMAIN[] = "ambiesoft.mooo.com";
 Q_CONSTEXPR char APPNAME[] = "BupPlay";
-Q_CONSTEXPR char APPVERSION[] = "0.0.1";
+Q_CONSTEXPR char APPVERSION[] = "0.0.3";
 
 Q_CONSTEXPR char KEY_STYLE[] = "style";
 Q_CONSTEXPR char KEY_VOLUME[] = "volume";
